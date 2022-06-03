796cef8654eecf52af1fc1bab61d48747d6ae12d
hongshinn <912372447@qq.com>
2022-06-03 15:47:56 +0800

    Changes

--- hpsl/GameFile.py
+++ hpsl/GameFile.py
@@ -1,7 +1,8 @@
 import json
 import os
 import hpsl.Network
-
+import sys
+import platform
 
 class Download:
     def __init__(self):
@@ -38,13 +39,53 @@ class Download:
                 sha1 = str(download_parameters['size'])
                 url = str(download_parameters['url']).replace('https://libraries.minecraft.net/', self.lib_api)
                 try:
-                    save_path = os.path.join(save_path, minecraft_dir, 'libraries')
-                    for path_fragment in path.split('/'):
-                        save_path = os.path.join(save_path, path_fragment)
-                    if not os.path.exists(os.path.dirname(save_path)):
-                        os.makedirs(os.path.dirname(save_path))
-                    if not os.path.exists(save_path):
-                        hpsl.Network.download(url, save_path)
+                    self.__download_lib_file(minecraft_dir, path, save_path, url)
 
                 except BaseException as err:
-                    print(err)
+                    raise err
+            if 'classifiers' in lib_json:
+
+                if sys.platform.startswith('linux'):
+                    # linux
+                    download_type = 'natives-linux'
+
+                elif sys.platform.startswith('darwin'):
+                    # mac os
+                    download_type = 'natives-osx'
+
+                elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+                    # win
+                    if 'natives-windows' in lib_json['downloads']['classifiers']:
+                        download_type = 'natives-windows'
+                    elif platform.architecture()[0] == '64bit':
+                        download_type = 'natives-windows-64'
+                    elif platform.architecture()[0] == '32bit':
+                        download_type = 'natives-windows-32'
+                    else:
+                        download_type = ''
+
+                else:
+                    download_type = ''
+
+                download_parameters = lib_json['classifiers'][download_type]
+                path = str(download_parameters['path'])
+                sha1 = str(download_parameters['size'])
+                url = str(download_parameters['url']).replace('https://libraries.minecraft.net/', self.lib_api)
+
+    def __download_lib_file(self, minecraft_dir, path, save_path, url):
+        save_path = os.path.join(save_path, minecraft_dir, 'libraries')
+        for path_fragment in path.split('/'):
+            save_path = os.path.join(save_path, path_fragment)
+        if not os.path.exists(os.path.dirname(save_path)):
+            os.makedirs(os.path.dirname(save_path))
+        if not os.path.exists(save_path):
+            hpsl.Network.download(url, save_path)
+
+    def download_client(self, file_json: json, minecraft_dir: str, name: str):
+        try:
+            save_path = os.path.join(minecraft_dir, 'versions', name, '{}.jar'.format(name))
+
+            hpsl.Network.download(file_json['downloads']['client']['url'], save_path)
+        except BaseException as err:
+            raise err
+
